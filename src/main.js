1e9d26f448738f02a1e126194d2452cde1fdf0fa
Gabriel Bertola Bocca <digitalbocca@gmail.com>
2023-04-21 01:28:40 -0300

    Configura pinia

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import { createApp } from 'vue'
 import router from '@/router'
+import store from '@/stores'
 import AppComponent from '@/components/app'
 import '@/sass/styles.sass'
 
 const app = createApp(AppComponent)
 
 app.use(router)
+app.use(store)
 
 app.mount('#app')
